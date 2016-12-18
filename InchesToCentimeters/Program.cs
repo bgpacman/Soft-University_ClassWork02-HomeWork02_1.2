b0f6dba77e248ee197549feffe99e6e259ef65da
Lubcho Milushev <lubcho.milushev@gmail.com>
2016-12-18 02:41:42 +0200

    ClassWork02-HomeWork02_1.3
    C# 1.3

---
+++ InchesToCentimeters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InchesToCentimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inches = ");
+            var inches = double.Parse(Console.ReadLine());
+            var centimeters = inches * 2.54;
+
+            Console.Write("Centimeters = ");
+            Console.WriteLine(centimeters);
+        }
+    }
+}
---
+++ RadiansToDegrees/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadiansToDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rad = double.Parse(Console.ReadLine());
+            //  degrees = radians*180/Math.PI; 
+            var c = (rad * 180 / Math.PI);
+
+            Console.WriteLine(Math.Round(c, 0));
+        }
+    }
+}
